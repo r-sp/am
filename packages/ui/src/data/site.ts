f316dfa72e2ae8169ab98fe794f655477d4431fc
r-sp <36327242+r-sp@users.noreply.github.com>
2024-09-05 23:48:12 +0700

    add configurable base layout

---
+++ packages/ui/src/data/site.ts
@@ -0,0 +1,9 @@
+interface SitePreferences {
+  lang: 'en-US' | 'en-ID';
+  dir: 'ltr' | 'rtl';
+}
+
+export const siteConfig: SitePreferences = {
+  lang: 'en-ID',
+  dir: 'ltr',
+};
--- packages/ui/src/layouts/base.tsx
+++ packages/ui/src/layouts/base.tsx
@@ -1,10 +1,12 @@
+import { siteConfig as base } from '../data/site';
+
 export default function BaseLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en-ID">
+    <html lang={base.lang} dir={base.dir}>
       <body>{children}</body>
     </html>
   );
